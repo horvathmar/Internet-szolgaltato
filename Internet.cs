62147e0cd106c5dd9decff20928bc3123c0609f5
Horvath Mark <horvath.mark@students.jedlik.eu>
2025-02-14 08:35:41 +0100

    new user added

---
+++ Internet.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Internet_szolgaltato
+{
+    internal class Internet
+    {
+
+        public string Szerver { get; set; }
+        public string Router { get; set; }
+        public string Ugyfel { get; set; }
+        public string Internetcsomag { get; set; }
+        public Internet(string szerver, string router, string ugyfel, string internetcsomag)
+        {
+            Szerver = szerver;
+            Router = router;
+            Ugyfel = ugyfel;
+            Internetcsomag = internetcsomag;
+        }
+
+    }
+}
--- Program.cs
+++ Program.cs
@@ -4,7 +4,22 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("teszt");
+            Console.WriteLine("Új fiók létrehozása");
+            Console.Write("Név: ");
+            string nev1 = Console.ReadLine();
+            Console.Write("Jelszó: ");
+            int j1 = int.Parse(Console.ReadLine());
+            try
+            {
+                Felhasznalo p1 = new Felhasznalo(nev1, j1);
+                Console.WriteLine($"{nev1} sikeresen csatlakozott a hálózathoz.");
+                //Felhasznalo p2 = new Felhasznalo("Gezuka", 1234567);
+
+            }
+            catch (Exception ex) 
+            {
+                Console.WriteLine(ex.Message);
+            }
 
         }
     }
